89c68f9e8d96cb76ac4d4ad5a78c816e4253615b
Isaac Victoria <isaac127@live.com.mx>
2022-06-17 10:32:32 -0500

    Funcionalidad de quizZoom trasladada a carpeta JS

---
+++ Milo/js/quizZoom.js
@@ -0,0 +1,6 @@
+function respCorrecta() {  
+    alert (" Respuesta correcta, Felicidades");  
+}  
+function respIncorrecta() {  
+    alert (" No es la respuesta correcta, intente con otra opción");  
+}
