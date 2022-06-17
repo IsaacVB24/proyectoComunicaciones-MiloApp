5e9d9787d29b96d471c3087452388fd65d8c5a5b
Isaac Victoria <isaac127@live.com.mx>
2022-06-17 18:36:29 -0500

    Botones modificados y 100% funcionales

--- Milo/js/quizZoom.js
+++ Milo/js/quizZoom.js
@@ -73,7 +73,7 @@ function acabarTodoElQuiz(){
     if(quiz1 == 1 & quiz2 == 1){
         document.getElementById("preguntaReunion").style.display = "none";
         document.getElementById("preguntaCompPantalla").style.textDecoration = "initial";
-        document.getElementById("preguntaCompPantalla").innerHTML = "¡Felicidades! Completaste todos los Quizes de esta sección.<br><br><table style='font-weight: initial; font-size: large;'><tr><td><button onclick='recargar();' href='../html/quizZoom.html'>Repetir Quiz</button></td><td><button style='margin-left:1%;'><a sytle = '  border-radius: 50%;'href='../html/index.html'>Regresar al menú principal</a></button></td></tr></table>";
+        document.getElementById("preguntaCompPantalla").innerHTML = "¡Felicidades! Completaste todos los Quizes de esta sección.<br><br><table style='font-weight: initial; font-size: large;'><tr><td><button onclick='recargar();' href='../html/quizZoom.html' style='margin-left:5%;border-radius: 20px;'>Repetir Quiz</button></td><td><button style='margin-left:5%;border-radius: 20px;'><a sytle = '  ' href='../html/index.html'>Regresar al menú principal</a></button></td></tr></table>";
     }
 }
 function recargar(){
