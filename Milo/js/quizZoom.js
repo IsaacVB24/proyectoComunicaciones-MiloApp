67361a3503345c38574a64f369fefd51eec8f1d9
Isaac Victoria <isaac127@live.com.mx>
2022-06-17 11:03:52 -0500

    Funcionalidad de quizZoom incompleta

--- Milo/js/quizZoom.js
+++ Milo/js/quizZoom.js
@@ -1,5 +1,7 @@
 function respCorrecta() {  
-    alert (" Respuesta correcta, Felicidades");  
+    alert (" Respuesta correcta, Felicidades");
+    setTimeout("repCorrecta()", 3000);
+    clearTimeout("respCorrecta()");
 }  
 function respIncorrecta() {  
     alert (" No es la respuesta correcta, intente con otra opción");  
