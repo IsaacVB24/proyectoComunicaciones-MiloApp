7568e02cb5423acd3ef0b579f313dcfb722e1978
Isaac Victoria <isaac127@live.com.mx>
2022-06-17 12:40:38 -0500

    Respuesta correcta e incorrecta actualizada

--- Milo/js/quizZoom.js
+++ Milo/js/quizZoom.js
@@ -1,6 +1,10 @@
 /*
     background-color: #cae8ca;
     border: 2px solid #3d863f;
+
+    Respuesta incorrecta
+    background-color: #e8caca;
+    border: 2px solid #863d3d;
 */
 
 
@@ -8,6 +12,13 @@ function respCorrecta(){
     document.getElementById("mensaje").innerHTML = "¡Felicidades! Respuesta correcta.";
     document.getElementById("mensaje").style.backgroundColor = "#cae8ca";
     document.getElementById("mensaje").style.border = "2px solid #3d863f";
+    $("#mensaje").fadeIn(2000);
+    $("#mensaje").fadeOut(7000);
+}
+function respIncorrecta(){            
+    document.getElementById("mensaje").innerHTML = "¡Intente de nuevo! Respuesta incorrecta.";
+    document.getElementById("mensaje").style.backgroundColor = "#e8caca";
+    document.getElementById("mensaje").style.border = "2px solid #863d3d";
     $("#mensaje").fadeIn(2000);
     $("#mensaje").fadeOut(7000);
 }
